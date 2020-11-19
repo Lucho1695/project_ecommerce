6e156cc0d2896ef6ace539bac535258e53b0fba0
Luis Carlos Garcia <lcgb11@hotmail.com>
2020-11-18 23:58:48 -0500

    Que se haga la luz

---
+++ db/migrate/2020101100004_create_products.rb
@@ -0,0 +1,14 @@
+class CreateProducts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :products do |t|
+      t.integer :user_id
+      t.integer :client_id
+      t.string :description
+      t.text :photo, limit: 6.megabytes - 1
+      t.float :price
+
+      t.timestamps
+    end
+    add_index :products, [:client_id, :user_id, :price]
+  end
+end
